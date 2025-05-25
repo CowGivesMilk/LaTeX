0ba5a6b0471422c5f13ad99eaab9a11a95caf576
CowGivesMilk <nimeshpoudel89@gmail.com>
2025-05-25 11:31:12 +0545

    Fixed rect and removed logo from cover page

--- DSAP LAB 1/code/rect.m
+++ DSAP LAB 1/code/rect.m
@@ -2,10 +2,8 @@ a = 6;
 t = -10:0.01:10;
 rect_t = zeros(size(t));
 for k = 1:length(t)
-    if abs(t(k)) < a/2
+    if abs(t(k)) <= a/2
         rect_t(k) = 1;
-    elseif abs(t(k)) == a/2
-        rect_t(k) = 0.5;
     else
         rect_t(k) = 0;
     endif
@@ -19,10 +17,8 @@ xlabel('t'); ylabel('rect(t/a)');
 n = -10:10;
 rect_n = zeros(size(n));
 for k = 1:length(n)
-    if abs(n(k)) < a/2
+    if abs(n(k)) <= a/2
         rect_n(k) = 1;
-    elseif abs(n(k)) == a/2
-        rect_n(k) = 0.5;
     else
         rect_n(k) = 0;
     endif
